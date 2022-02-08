5eb51ff480a346d3f7a6a480a20990223c51cd5a
代小呆 <diauweb@live.com>
2022-02-08 12:13:11 +0000

    feat: build folder

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 const fs = require('fs-extra')
-const path = require('path')
 const cp = require('child_process')
+
+const { generate } = require('./generate')
+
 const exec = (cmd, options) => {
   const k = cp.spawn(cmd, [], { shell: true, stdio: 'inherit', ...options })
   return new Promise((resolve, reject) => {
@@ -11,30 +13,33 @@ const exec = (cmd, options) => {
 const jamstackRepo = 'https://github.com/TeyvatLore/TeyvatLore'
 
 async function cloneRepo () {
-  await exec(`git clone --depth=1 ${jamstackRepo} ./repo`)
-  await fs.rm('./repo/content', { recursive: true })
-  await fs.symlink(path.resolve('./content/'), path.resolve('./repo/content/'))
+  await exec(`git clone --depth=1 ${jamstackRepo} ./build/repo`)
+  await fs.rm('./build/repo/content', { recursive: true })
 }
 
 async function clean () {
-  if (fs.existsSync('./dist')) {
-    await fs.rm('./dist', { recursive: true })
-    await fs.mkdir('./dist')
+  if (fs.existsSync('./build')) {
+    await fs.rm('./build', { recursive: true })
   }
-  if (fs.existsSync('./repo')) {
-    await fs.rm('./repo', { recursive: true })
-  }
+  await fs.mkdir('./build/content_generated', { recursive: true })
 }
 
 async function build () {
-  await exec('yarn install', { cwd: './repo' })
-  await exec('yarn generate', { cwd: './repo' })
+  await exec('yarn install', { cwd: './build/repo' })
+  await exec('yarn generate', { cwd: './build/repo' })
 
-  await fs.copy('./repo/dist', './dist')
+  await fs.copy('./build/repo/dist', './dist')
+}
+
+async function makeContent () {
+  await generate()
+  await fs.copy('./content', './build/repo/content')
+  await fs.copy('./build/content_generated', './build/repo/content')
 }
 
 (async () => {
   await clean()
   await cloneRepo()
+  await makeContent()
   await build()
 })()
